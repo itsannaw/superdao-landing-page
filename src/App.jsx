940b6ed292806bcf8ab86ce5ff074fe9897ce5c1
Anna Galkevich <emikot321@gmail.com>
2023-08-30 21:51:32 +0300

    Header

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Header from './assets/layers/Header'
+
+function App() {
+
+  return (
+      <div className='App'>
+          <div className=''>
+              <Header />
+          </div>
+       </div>
+    
+  )
+}
+
+export default App;
---
+++ src/assets/layers/Header.jsx
@@ -0,0 +1,18 @@
+
+
+
+
+const Header = () => {
+    return ( 
+        <section>
+            <div>
+                <img src="header/stars.png" alt="stars" />
+                <div>
+
+                </div>
+            </div>
+        </section>
+     );
+}
+
+export default Header ;
